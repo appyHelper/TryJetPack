57c046530f1ae97bd2179d452a2307fcffed5e89
Lakshmi <lakshmi@a483e79b9fe0.ant.amazon.com>
2020-10-05 17:23:51 -0500

    notification added

--- app/src/main/java/com/example/tryjetpack/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/tryjetpack/view/activity/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.tryjetpack.view.activity
 
 import android.app.AppComponentFactory
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,6 +35,8 @@ class MainActivity : AppCompatActivity() {
 
 @Inject lateinit var dressViewModelFactory: DressViewModelFactory
     lateinit var viewModel: DressViewModel
+    private val channelId= "channel1"
+    private var notificationManager:NotificationManager?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,11 +57,32 @@ class MainActivity : AppCompatActivity() {
 
         dressAddButton.setOnClickListener {
             hideSoftKeyboard()
-
+            createChannel(channelId,"Dress Added","New Dress Added")
+            displayNotification()
         }
 
     }
 
+    private fun displayNotification(){
+       val notificationId =45
+        notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification: Notification =NotificationCompat.Builder(this,channelId).setContentTitle("notification title")
+            .setContentText("dress is ready content")
+            .setSmallIcon(R.drawable.greysaree_background)
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .build()
+     notificationManager?.notify(notificationId,notification)
+    }
+
+    private fun createChannel(id:String,name:String,channelDescription:String){
+   if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+      val importance = NotificationManager.IMPORTANCE_HIGH
+       val channel =NotificationChannel(id,name, importance).apply {
+           description=channelDescription
+       }
+       notificationManager?.createNotificationChannel(channel)
+   }
+    }
     private fun hideSoftKeyboard() {
         if (currentFocus != null && currentFocus is EditText) {
             val inputManager:InputMethodManager =getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
